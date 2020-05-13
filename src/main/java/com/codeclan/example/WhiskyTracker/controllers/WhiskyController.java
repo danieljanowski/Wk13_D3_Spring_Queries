acf9199869c9ade76292f78a4d2520245928d734
Daniel Janowski <daniel.janowski@gmail.com>
2020-05-13 22:26:24 +0100

    MVP

---
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -0,0 +1,44 @@
+package com.codeclan.example.WhiskyTracker.controllers;
+
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class WhiskyController {
+
+    @Autowired
+    private WhiskyRepository whiskyRepository;
+
+    @GetMapping(value = "/whiskies")
+    public ResponseEntity<List<Whisky>> getAllWhiskies(
+            @RequestParam(name = "year", required = false) Integer year,
+            @RequestParam(name = "region", required = false) String region,
+            @RequestParam(name = "age", required = false) Integer age,
+            @RequestParam(name = "distillery", required = false) String distname
+    ){
+        if (year != null){
+            return new ResponseEntity<List<Whisky>>(whiskyRepository.findByYear(year),HttpStatus.OK);
+        }
+
+        if (region != null){
+            return new ResponseEntity<List<Whisky>>(whiskyRepository.findByDistilleryRegion(region), HttpStatus.OK);
+        }
+
+        if (age != null & distname != null){
+            return new ResponseEntity<List<Whisky>>(whiskyRepository.findByAgeAndDistilleryName(age, distname), HttpStatus.OK);
+        }
+
+        List<Whisky> foundWhiskies = whiskyRepository.findAll();
+        return new ResponseEntity<List<Whisky>>(foundWhiskies, HttpStatus.OK);
+    }
+
+}
