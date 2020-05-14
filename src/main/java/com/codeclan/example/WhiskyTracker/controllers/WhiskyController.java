a1140e34db1ab23d266d7fdb9961b71bd3443c6f
Daniel Janowski <daniel.janowski@gmail.com>
2020-05-14 08:37:53 +0100

    MVP, 5/5 Extensions done Whisky CRU routes

--- src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -5,10 +5,7 @@ import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -50,6 +47,27 @@ public class WhiskyController {
     public ResponseEntity<List<Whisky>> getWhiskies(){
         return new ResponseEntity<List<Whisky>>(whiskyRepository.findByNameStartingWith("The"), HttpStatus.OK);
     }
+
+    @GetMapping(value = "/whisky/{id}")
+    public ResponseEntity getWhisky(@PathVariable Long id){
+        return new ResponseEntity<>(whiskyRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/whiskies/{id}")
+    public ResponseEntity<Whisky> putWhisky(@RequestBody Whisky whisky, @PathVariable Long id){
+        if (whisky.getId().longValue() != id){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        whiskyRepository.save(whisky);
+        return new ResponseEntity<>(whisky, HttpStatus.CREATED);
+    }
+
+    @PostMapping(value = "whiskies")
+    public ResponseEntity getWhisky(@RequestBody Whisky whisky){
+        whiskyRepository.save(whisky);
+        return new ResponseEntity<>(whisky, HttpStatus.CREATED);
+    }
+
 
 
 }
